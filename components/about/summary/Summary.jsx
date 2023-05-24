f982d5c67d3e306f93f8014069a7febce71e2880
Shidqi <rifatpangestu87@gmail.com>
2023-05-25 00:10:23 +0700

    refactor: content summary in about page

--- components/about/summary/Summary.jsx
+++ components/about/summary/Summary.jsx
@@ -3,61 +3,42 @@ import Content from "../../section/Content";
 import { useEffect } from "react";
 import aosAnimate from "../../utils/aosAnimate";
 
-export default function Summary({ handleSeeFull, isSeeFull }) {
-    useEffect(() => {
-        aosAnimate(".about");
-    });
+const Link = ({ link, children }) => {
+  return (
+    <a
+      href={link}
+      target="_blank"
+      rel="noreferrer"
+      className="text-sky-500 font-medium hover:text-sky-600 dark:hover:text-sky-400 hover:underline hover:underline-offset-4 transition"
+    >
+      {children}
+    </a>
+  );
+};
 
-    return (
-        <div className="mb-4">
-            <Title mb={4}>Summary</Title>
-            <Content isSummary={true}>
-                I am a software/frontend developer with experients +10 months
-                and a fresh graduate from Politeknik Negeri Jakarta in
-                Electrical Engineering major last year. My last work experience
-                is as a Frontend Developer in LingoTalk which education
-                technology startup that focuses on foreign language learning for
-                elementary school students.{" "}
-                {!isSeeFull && (
-                    <span
-                        className="cursor-pointer hover:underline underline-offset-2 text-sky-500 hover:text-sky-600 dark:hover:text-sky-400"
-                        onClick={handleSeeFull}
-                    >
-                        See More
-                    </span>
-                )}
-                <span
-                    className={`${
-                        isSeeFull
-                            ? "h-max visible opacity-100"
-                            : "h-0 hidden opacity-0"
-                    }`}
-                >
-                    Currently, I also learn in online classes named Dicoding
-                    Academy to deepen my knowledge of web development roadmap. I
-                    have a mission to be great in my field so that I can share
-                    knowledge more widely.
-                </span>
-            </Content>
-            <Content
-                style={{
-                    opacity: isSeeFull ? 1 : 0,
-                    visibility: isSeeFull ? "visible" : "hidden",
-                    height: isSeeFull ? "max-content" : "0",
-                }}
-                isSummary={true}
-            >
-                I am very passionate about technology and computers. I am
-                reliable, can work in a team, and can learn quickly my interests
-                are in frontend development. In my free time, I keep up with the
-                news of computer technology, play games, and read books.{" "}
-                <span
-                    className="cursor-pointer text-sky-500 hover:underline underline-offset-2 hover:text-sky-600 dark:hover:text-sky-400"
-                    onClick={handleSeeFull}
-                >
-                    See Less
-                </span>
-            </Content>
-        </div>
-    );
+export default function Summary() {
+  useEffect(() => {
+    aosAnimate(".about");
+  });
+
+  return (
+    <div className="mb-4">
+      <Title mb={4}>Summary</Title>
+      <Content isSummary={true}>
+        I am a frontend developer with +1 year of experience and graduated from
+        Jakarta State Polytechnic majoring in Electrical Engineering. Currently,
+        I work as a Frontend Developer in an IT consulting company. I am used to
+        translating business processes into code and producing products such as
+        company landing pages, web apps, or web dashboards. <br />
+        <br />
+        Outside of professional activities, I often deepen my web knowledge by
+        studying online classes such as the Dicoding Academy or the YouTube
+        channel, experimenting with it, and sharing it in my own GitHub profile{" "}
+        <Link link="https://github.com/shidqirifat">@shidqirifat</Link>.
+        <br />
+        <br />I have a mission to be great in my field so that I can share
+        knowledge more broadly.
+      </Content>
+    </div>
+  );
 }
